88c5b4d57ed8878b26730b09c2d596111134498a
diegoshe <belov-93201093@mail.ru>
2019-11-16 05:09:30 +0300

    first

---
+++ precache-manifest.829aadd8cfd5dd9d28196fc62f25e94c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ca94113a14270b0128a248cdac031e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd7a1c1516e67b7ba0ee",
+    "url": "/static/css/main.89d5fe65.chunk.css"
+  },
+  {
+    "revision": "57c5c065bae94779d803",
+    "url": "/static/js/2.d5a7b0a1.chunk.js"
+  },
+  {
+    "revision": "dd7a1c1516e67b7ba0ee",
+    "url": "/static/js/main.92b77f96.chunk.js"
+  },
+  {
+    "revision": "0ac2b0fd5fd4af3be109",
+    "url": "/static/js/runtime-main.8896086d.js"
+  },
+  {
+    "revision": "88ceb18ec700d5eed600d005cd92463a",
+    "url": "/static/media/avatar2.88ceb18e.jpg"
+  },
+  {
+    "revision": "46a9582ba5e246c400d5f6d6705a9d73",
+    "url": "/static/media/networck.46a9582b.jpg"
+  },
+  {
+    "revision": "1d0fb8a38403808fd722f1d78e8b33e8",
+    "url": "/static/media/store.1d0fb8a3.jpg"
+  },
+  {
+    "revision": "f4affb70c9a7a1009e46db4310b0807a",
+    "url": "/static/media/todolist.f4affb70.jpg"
+  }
+]);
